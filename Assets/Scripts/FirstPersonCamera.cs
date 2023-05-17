6fa1f31623cb89ce19fb1bf0d2e359e9d5204318
ilyablokh <ilyablokh@gmail.com>
2023-05-17 16:11:40 +0300

    add first person camera

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [SerializeField] private float _mouseSensitivity = 10f;
+    public Transform Target { get; set; }
+
+    private float _verticalRotation;
+    private float _horizontalRotation;
+
+    private void LateUpdate()
+    {
+        if (Target == null)
+            return;
+
+        transform.position = Target.position;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        _verticalRotation -= mouseY * _mouseSensitivity;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -70f, 70f);
+
+        _horizontalRotation += mouseX * _mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(_verticalRotation, _horizontalRotation, 0);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,24 @@
 ﻿using Fusion;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] private float _playerSpeed = 2f;
     private CharacterController _controller;
+    private Camera _camera;
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
+    }
+
+    public override void Spawned()
+    {
+        _camera = Camera.main;
+        Debug.Assert(_camera != null, nameof(_camera) + " != null");
+        _camera.GetComponent<FirstPersonCamera>().Target = GetComponent<NetworkTransform>().InterpolationTarget;
     }
 
     public override void FixedUpdateNetwork()
@@ -17,13 +26,12 @@ public class PlayerMovement : NetworkBehaviour
         if (HasStateAuthority == false)
             return;
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Runner.DeltaTime * _playerSpeed;
+        Quaternion cameraRotationY = Quaternion.Euler(0, _camera.transform.rotation.eulerAngles.y, 0);
+        Vector3 move = cameraRotationY * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Runner.DeltaTime * _playerSpeed;
         _controller.Move(move);
 
-        if (move != Vector3.zero)
-        {
+        if (move != Vector3.zero) 
             gameObject.transform.forward = move;
-        }
 
     }
 }
