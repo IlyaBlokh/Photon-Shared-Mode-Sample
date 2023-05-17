a5eac044ca43e988cdf2aa40fc0c018f5d20dd0e
ilyablokh <ilyablokh@gmail.com>
2023-05-17 14:24:46 +0300

    spawn a player prefab

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,12 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    [SerializeField] private GameObject playerPrefab;
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer) 
+            Runner.Spawn(playerPrefab, new Vector3(0, 1, 0), Quaternion.identity, player);
+    }
+}
