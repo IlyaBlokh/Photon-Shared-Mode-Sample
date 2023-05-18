da0591d69c57a6456f98d6800ca018fb936a1ce2
ilyablokh <ilyablokh@gmail.com>
2023-05-18 10:14:48 +0300

    change player color and sync property

---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,23 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class PlayerColor : NetworkBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+
+    [Networked(OnChanged = nameof(NetworkColorChanged))]
+    public Color NetworkedColor { get; set; }
+    
+    private void Update()
+    {
+        if (HasStateAuthority && Input.GetKeyDown(KeyCode.E))
+        {
+            NetworkedColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
+    }
+    
+    private static void NetworkColorChanged(Changed<PlayerColor> changed)
+    {
+        changed.Behaviour._meshRenderer.material.color = changed.Behaviour.NetworkedColor;
+    }
+}
